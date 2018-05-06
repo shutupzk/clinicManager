1c4fd37ebf7c2ace579a8c79748f75654ee62f73
ruan <894159898@qq.com>
2018-05-06 20:08:36 +0800

    修改

--- modules/treatment/screens/addmission/index.js
+++ modules/treatment/screens/addmission/index.js
@@ -75,23 +75,23 @@ class AddmisionScreen extends Component {
         }
       })
     }
-    Router.push('/treatment/reception')
+    Router.push('/treatment/admission/reception')
   }
 
   receptionOperation(clinic_triage_patient_id) {
     triagePatientsSelect({ clinic_triage_patient_id })
-    Router.push('/treatment/reception')
+    Router.push('/treatment/admission/reception')
   }
 
   formatWaittingTime(time) {
     if (time > 1440) {
-      return `${Math.floor(time / 1440)}天${Math.floor((time % 1440) / 60)}时${time % 60}分`
+      return `${Math.floor(time / 1440)}天${Math.floor((time % 1440) / 60)}小时${time % 60}分钟`
     }
     if (time > 60) {
-      return `${Math.floor(time / 60)}时${time % 60}分`
+      return `${Math.floor(time / 60)}小时${time % 60}分钟`
     }
     if (time <= 0) return `小于1分钟`
-    return time
+    return `${time}分钟`
   }
 
   // 显示待接诊列表
