b64831e9a7a5f8a2eaf1d1fa8758dae706a6bf50
康  查 <956468883@qq.com>
2018-05-14 00:07:24 +0800

    修改

--- ducks/prescription_chinese_patient.js
+++ ducks/prescription_chinese_patient.js
@@ -15,6 +15,7 @@ export function prescriptionChinesePatients(state = initState, action = {}) {
 }
 
 export const PrescriptionChinesePatientCreate = ({
+  id,
   clinic_triage_patient_id,
   route_administration_id,
   frequency_id,
@@ -27,6 +28,7 @@ export const PrescriptionChinesePatientCreate = ({
 }) => async dispatch => {
   try {
     const data = await request('/triage/PrescriptionChinesePatientCreate', {
+      id,
       clinic_triage_patient_id,
       route_administration_id,
       frequency_id,
@@ -52,71 +54,72 @@ export const PrescriptionChinesePatientGet = ({ clinic_triage_patient_id }) => a
     const data = await request('/triage/PrescriptionChinesePatientGet', {
       clinic_triage_patient_id
     })
-    console.log('PrescriptionChinesePatientGet ======', data)
-    if (data.code !== '200' || !data.data) return {}
-    let info = data.data
-    let docs = info.items || []
-    let json_data = {}
-    let unitJson = {}
-    let frequencyJson = {}
-    let routeJson = {}
-    for (let doc of docs) {
-      const {
-        drug_stock_id,
-        drug_name,
-        specification,
-        stock_amount,
-        packing_unit_id,
-        packing_unit_name,
-        once_dose_unit_id,
-        once_dose_unit_name,
-        route_administration_id,
-        route_administration_name,
-        frequency_id,
-        frequency_name,
-        type
-      } = doc
-      json_data[doc.drug_stock_id] = {
-        drug_stock_id,
-        drug_name,
-        specification,
-        stock_amount,
-        packing_unit_id,
-        packing_unit_name,
-        once_dose_unit_id,
-        once_dose_unit_name,
-        route_administration_id,
-        route_administration_name,
-        frequency_id,
-        frequency_name,
-        type
+    if (data.code !== '200' || !data.data) return []
+    let array = data.data || []
+    for (let info of array) {
+      let docs = info.items || []
+      let json_data = {}
+      let unitJson = {}
+      let frequencyJson = {}
+      let routeJson = {}
+      for (let doc of docs) {
+        const {
+          drug_stock_id,
+          drug_name,
+          specification,
+          stock_amount,
+          packing_unit_id,
+          packing_unit_name,
+          once_dose_unit_id,
+          once_dose_unit_name,
+          route_administration_id,
+          route_administration_name,
+          frequency_id,
+          frequency_name,
+          type
+        } = doc
+        json_data[doc.drug_stock_id] = {
+          drug_stock_id,
+          drug_name,
+          specification,
+          stock_amount,
+          packing_unit_id,
+          packing_unit_name,
+          once_dose_unit_id,
+          once_dose_unit_name,
+          route_administration_id,
+          route_administration_name,
+          frequency_id,
+          frequency_name,
+          type
+        }
+        if (packing_unit_id) unitJson[packing_unit_id] = { id: packing_unit_id, name: packing_unit_name }
+        if (once_dose_unit_id) unitJson[once_dose_unit_id] = { id: once_dose_unit_id, name: once_dose_unit_name }
+        if (frequency_id) frequencyJson[frequency_id] = { id: frequency_id, name: frequency_name }
+        if (route_administration_id) routeJson[route_administration_id] = { id: route_administration_id, name: route_administration_name }
+        dispatch({
+          type: 'DRUG_JSON_ADD',
+          json_data
+        })
+        dispatch({
+          type: 'DOSE_UNIT_ADD',
+          data: unitJson
+        })
+        dispatch({
+          type: 'FREQUENCY_ADD',
+          data: frequencyJson
+        })
+        dispatch({
+          type: 'ROUTE_ADMINISTRATION_ADD',
+          data: routeJson
+        })
       }
-      if (packing_unit_id) unitJson[packing_unit_id] = { id: packing_unit_id, name: packing_unit_name }
-      if (once_dose_unit_id) unitJson[once_dose_unit_id] = { id: once_dose_unit_id, name: once_dose_unit_name }
-      if (frequency_id) frequencyJson[frequency_id] = { id: frequency_id, name: frequency_name }
-      if (route_administration_id) routeJson[route_administration_id] = { id: route_administration_id, name: route_administration_name }
-      dispatch({
-        type: 'DRUG_JSON_ADD',
-        json_data
-      })
-      dispatch({
-        type: 'DOSE_UNIT_ADD',
-        data: unitJson
-      })
-      dispatch({
-        type: 'FREQUENCY_ADD',
-        data: frequencyJson
-      })
-      dispatch({
-        type: 'ROUTE_ADMINISTRATION_ADD',
-        data: routeJson
-      })
     }
     dispatch({
       type: PRESCRIPTION_CHINESE_PATIENT_ADD,
-      data: docs
+      data: array
     })
-    return info
+    return array
   } catch (e) {
     console.log(e)
     return []
