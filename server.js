0b642f3074c822173a2d28464c64350e257a4457
ruan <894159898@qq.com>
2018-05-14 23:03:43 +0800

    修改端口

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const next = require('next')
-const config = require('./config')
 
-const port = parseInt(process.env.PORT, 10) || config.PORT
+const port = parseInt(process.env.PORT, 10) || 9000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -25,8 +24,8 @@ app.prepare().then(() => {
   server.get('*', (req, res) => {
     return handle(req, res)
   })
-  
-  server.listen(port, (err) => {
+
+  server.listen(port, err => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}/login`)
   })
