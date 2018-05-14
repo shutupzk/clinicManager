071828bebad6a6ae47fb9808d0b429909019257f
ruan <894159898@qq.com>
2018-05-14 22:59:51 +0800

    端口

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const next = require('next')
+const config = require('./config')
 
-const port = parseInt(process.env.PORT, 10) || 9151
+const port = parseInt(process.env.PORT, 10) || config.PORT
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
