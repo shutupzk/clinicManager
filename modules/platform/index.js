02c7472d04d8be95b8ba2a5e59bfed78e416e8d1
ruan <894159898@qq.com>
2018-05-28 22:33:21 +0800

    设置

--- modules/platform/index.js
+++ modules/platform/index.js
@@ -1,5 +1,6 @@
 import ClinicAddScreen from './screens/clinic/clinic_add_screen'
 import ClinicListScreen from './screens/clinic/clinic_list_screen'
 import BusinessClinicListScreen from './screens/business'
+import BusinessClinicPermissionScreen from './screens/business/permission_screen'
 
-export { ClinicAddScreen, ClinicListScreen, BusinessClinicListScreen }
+export { ClinicAddScreen, ClinicListScreen, BusinessClinicListScreen, BusinessClinicPermissionScreen }
