8d8b1fbac8f2bb3dcc05d011e903a0dd30ab3e91
康  查 <956468883@qq.com>
2018-06-03 21:54:32 +0800

    xiugai

---
+++ ducks/diagnosis_treatment_patient.js
@@ -0,0 +1,59 @@
+import { request } from './request'
+const LABORATORY_PATIENT_ADD = 'LABORATORY_PATIENT_ADD'
+
+const initState = {
+  data: []
+}
+
+export function laboratoryPatients(state = initState, action = {}) {
+  switch (action.type) {
+    case LABORATORY_PATIENT_ADD:
+      return { ...state, data: action.data }
+    default:
+      return state
+  }
+}
+
+export const DiagnosisTreatmentPatientCreate = ({ clinic_triage_patient_id, personnel_id, items }) => async dispatch => {
+  try {
+    const data = await request('/triage/DiagnosisTreatmentPatientCreate', {
+      clinic_triage_patient_id, personnel_id, items: JSON.stringify(items)
+    })
+    console.log(clinic_triage_patient_id, personnel_id, items)
+    console.log('data ======', data)
+    if (data.code !== '200') return data.msg
+    return null
+  } catch (e) {
+    console.log(e)
+    return e.message
+  }
+}
+
+export const LaboratoryPatientGet = ({ clinic_triage_patient_id }) => async dispatch => {
+  try {
+    const data = await request('/triage/LaboratoryPatientGet', {
+      clinic_triage_patient_id
+    })
+    console.log(clinic_triage_patient_id)
+    console.log('data ======', data)
+    if (data.code !== '200') return []
+    let docs = data.data || []
+    let json = {}
+    for (let doc of docs) {
+      const { clinic_laboratory_id, laboratory_name } = doc
+      json[clinic_laboratory_id] = { clinic_laboratory_id, laboratory_name }
+    }
+    dispatch({
+      type: LABORATORY_PATIENT_ADD,
+      data: docs
+    })
+    dispatch({
+      type: 'LABORATORY_PROJECT_ADD',
+      data: json
+    })
+    return docs
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
--- modules/treatment/screens/addmission/reception_screen.js
+++ modules/treatment/screens/addmission/reception_screen.js
@@ -8,16 +8,23 @@ import LaboratoryScreen from './components/laboratory'
 import ExamineScreen from './components/examine'
 import MaterialScreen from './components/material'
 import OtherScreen from './components/other'
-import { getAgeByBirthday } from '../../../../utils'
-import { Confirm } from '../../../../components'
-import { triageFinish } from '../../../../ducks'
+import { getAgeByBirthday, formatMoney } from '../../../../utils'
+import { Confirm, Select } from '../../../../components'
+import { triageFinish, queryDiagnosisTreatmentList, DiagnosisTreatmentPatientCreate } from '../../../../ducks'
 
 class RecptionScreen extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      pageType: 1
+      pageType: 1,
+      showConfirm: false,
+      clinic_diagnosis_treatment_id: ''
     }
+  }
+
+  componentDidMount() {
+    const { queryDiagnosisTreatmentList, clinic_id } = this.props
+    queryDiagnosisTreatmentList({ clinic_id, status: true, offset: 0, limit: 1000 })
   }
 
   showDataList() {
@@ -35,14 +42,178 @@ class RecptionScreen extends Component {
   }
 
   finishTriage() {
-    let { triage_personnel_id, clinic_triage_patient_id, triageFinish } = this.props
-    this.refs.myConfirm.confirm('确定完成接诊？', '', 'Success', async () => {
-      let error = await triageFinish({ clinic_triage_patient_id, recept_personnel_id: triage_personnel_id })
+    let { triage_personnel_id, clinic_triage_patient_id, triageFinish, DiagnosisTreatmentPatientCreate, personnel_id } = this.props
+    const { clinic_diagnosis_treatment_id } = this.state
+    this.refs.myAlert.confirm('确定完成接诊？', '', 'Success', async () => {
+      let error = await DiagnosisTreatmentPatientCreate({ personnel_id, clinic_triage_patient_id, items: [{ clinic_diagnosis_treatment_id: clinic_diagnosis_treatment_id + '', amount: '1' }] })
+      if (error) {
+        return this.refs.myAlert.alert('保存失败', error)
+      }
+      error = await triageFinish({ clinic_triage_patient_id, recept_personnel_id: triage_personnel_id })
       if (error) {
         return this.refs.myAlert.alert('结束失败', error)
       }
       Router.push('/treatment/admission')
     })
+  }
+
+  closeConfirm() {
+    this.setState({ showConfirm: false })
+  }
+
+  getTreatmentOption() {
+    const { diagnosisTreatments } = this.props
+    let array = []
+    for (let key in diagnosisTreatments) {
+      let { clinic_diagnosis_treatment_id, name, price } = diagnosisTreatments[key]
+      array.push({
+        value: clinic_diagnosis_treatment_id,
+        label: `${formatMoney(price)} （${name}）`,
+        ...diagnosisTreatments[key]
+      })
+    }
+    return array
+  }
+
+  getSelectValue(value, array) {
+    for (let obj of array) {
+      if (obj.value === value) {
+        return obj
+      }
+    }
+    return null
+  }
+
+  renderConfirmFinish() {
+    const type = 'Success'
+    const { showConfirm } = this.state
+    if (!showConfirm) return null
+    return (
+      <div className='mask'>
+        <div className='alertDiv'>
+          <div style={{ width: '509px', display: 'flex', flexDirection: 'row', height: '24px', marginLeft: '32px', marginRight: '32px', marginTop: '32px' }}>
+            <img src={`/static/icons/${type}.svg`} />
+            <div className='title'>结束就诊</div>
+          </div>
+          <div style={{ width: '509px', display: 'flex', flexDirection: 'row', flex: 1, marginLeft: '32px', marginRight: '32px', marginTop: '12px', marginBottom: '24px' }}>
+            <div className='content'>
+              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', height: '120px' }}>
+                <label>
+                  本次的诊疗费为：<b style={{ color: 'red' }}>*</b>
+                </label>
+                <div style={{ width: '200px', marginLeft: '20px' }}>
+                  <Select
+                    value={this.getSelectValue(this.state.clinic_diagnosis_treatment_id, this.getTreatmentOption())}
+                    onChange={item => {
+                      this.setState({ clinic_diagnosis_treatment_id: item.clinic_diagnosis_treatment_id })
+                    }}
+                    placeholder='选择'
+                    height={38}
+                    options={this.getTreatmentOption()}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style={{ width: '509px', display: 'flex', flexDirection: 'row', height: '32px', marginLeft: '32px', marginRight: '32px', marginBottom: '24px' }}>
+            <div style={{ flex: 1 }} />
+            <div className='buttonDiv buttonDivCancel' onClick={() => this.closeConfirm()}>
+              <span className='cancel'>取消</span>
+            </div>
+            <div className={`buttonDiv buttonDiv${type}`} onClick={() => this.finishTriage()}>
+              <span className={`span${type}`}>确定</span>
+            </div>
+          </div>
+        </div>
+        <style jsx='true'>
+          {`
+            .mask {
+              position: fixed;
+              display: flex;
+              width: 100%;
+              height: 100%;
+              background: rgba(0, 0, 0, 0.2);
+              top: 0;
+              left: 0;
+              z-index: 999;
+              align-items: center;
+              justify-content: center;
+            }
+            .alertDiv {
+              margin-bottom: 150px;
+              display: flex;
+              flex-direction: column;
+              width: 573px;
+              height: auto;
+              background: rgba(255, 255, 255, 1);
+              box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.2);
+              border-radius: 4px;
+            }
+            .title {
+              height: 24px;
+              font-size: 16px;
+              font-family: PingFangSC-Medium;
+              color: rgba(0, 0, 0, 0.85);
+              line-height: 24px;
+              flex: 1;
+              margin-left: 16px;
+            }
+            .content {
+              height: auto;
+              font-size: 14px;
+              font-family: PingFangSC-Regular;
+              color: rgba(0, 0, 0, 0.65);
+              line-height: 22px;
+              margin-left: 38px;
+            }
+            .buttonDiv {
+              width: 63px;
+              height: 30px;
+              border-radius: 4px;
+              cursor: pointer;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              margin-left: 8px;
+            }
+            .buttonDivSuccess {
+              background: rgba(42, 205, 200, 1);
+              border: 1px solid rgba(42, 205, 200, 1);
+            }
+            .buttonDivWarning {
+              background: rgba(42, 205, 200, 1);
+              border: 1px solid rgba(42, 205, 200, 1);
+            }
+            .buttonDivDanger {
+              background: rgba(0, 0, 0, 0.04);
+              border: 1px solid #d9d9d9;
+            }
+            .buttonDivCancel {
+              background: rgba(255, 255, 255, 1);
+              border: 1px solid #d9d9d9;
+            }
+            .buttonDiv span {
+              height: 22px;
+              font-size: 14px;
+              font-family: PingFangSC-Regular;
+              line-height: 22px;
+            }
+            .spanSuccess {
+              color: rgba(255, 255, 255, 1);
+            }
+            .spanWarning {
+              color: rgba(255, 255, 255, 1);
+            }
+            .spanDanger {
+              color: rgba(245, 34, 45, 1);
+            }
+            .cancel {
+              color: rgba(0, 0, 0, 0.65);
+            }
+          `}
+        </style>
+      </div>
+    )
   }
 
   render() {
@@ -73,7 +244,7 @@ class RecptionScreen extends Component {
             <a>{triagePatient.phone}</a>
           </div>
           <div className={'boxRight'}>
-            <button onClick={() => this.finishTriage()}>
+            <button onClick={() => this.setState({ showConfirm: true })}>
               <a>结束就诊</a>
             </button>
             <button>
@@ -140,8 +311,8 @@ class RecptionScreen extends Component {
           </span>
         </div>
         {this.showDataList()}
-        <Confirm ref='myAlert' isAlert />
-        <Confirm ref='myConfirm' />
+        {this.renderConfirmFinish()}
+        <Confirm ref='myAlert' />
         <style jsx='true'>
           {`
             .childTopBar {
@@ -184,8 +355,11 @@ const mapStateToProps = state => {
   return {
     triagePatients: state.triagePatients.data,
     clinic_triage_patient_id: state.triagePatients.selectId,
-    triage_personnel_id: state.user.data.id
+    triage_personnel_id: state.user.data.id,
+    personnel_id: state.user.data.id,
+    diagnosisTreatments: state.diagnosisTreatments.data,
+    clinic_id: state.user.data.clinic_id
   }
 }
 
-export default connect(mapStateToProps, { triageFinish })(RecptionScreen)
+export default connect(mapStateToProps, { triageFinish, queryDiagnosisTreatmentList, DiagnosisTreatmentPatientCreate })(RecptionScreen)
