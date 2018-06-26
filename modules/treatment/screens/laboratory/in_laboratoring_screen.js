ec7241ef6fd481d1be1cb8e8e3018e118fb06330
康  查 <956468883@qq.com>
2018-06-26 21:56:29 +0800

    xiugai

--- modules/treatment/screens/laboratory/in_laboratoring_screen.js
+++ modules/treatment/screens/laboratory/in_laboratoring_screen.js
@@ -5,6 +5,7 @@ import { LaboratoryTriageChecking } from '../../../../ducks'
 import moment from 'moment'
 import { getAgeByBirthday } from '../../../../utils'
 import { PageCard } from '../../../../components'
+import LaboraDetailScreen from './components/labora_detail_screen'
 
 class InLaboratoringScreen extends Component {
   constructor(props) {
@@ -12,23 +13,23 @@ class InLaboratoringScreen extends Component {
     this.state = {
       keyword: '',
       start_date: moment()
-      .add(-7, 'd')
-      .format('YYYY-MM-DD'),
+        .add(-7, 'd')
+        .format('YYYY-MM-DD'),
       end_date: moment()
-      .add(1, 'd')
-      .format('YYYY-MM-DD'),
+        .add(1, 'd')
+        .format('YYYY-MM-DD'),
       showMask: false,
       selItem: {}
     }
   }
 
   componentWillMount() {
-    this.getListData({offset: 0, limit: 6})
+    this.getListData({ offset: 0, limit: 6 })
   }
-	// 获取列表数据
-  getListData({offset = 0, limit = 6}) {
-    const {clinic_id, LaboratoryTriageChecking} = this.props
-    const {keyword, start_date, end_date} = this.state
+  // 获取列表数据
+  getListData({ offset = 0, limit = 6 }) {
+    const { clinic_id, LaboratoryTriageChecking } = this.props
+    const { keyword, start_date, end_date } = this.state
     let requestData = {
       clinic_id,
       offset,
@@ -45,9 +46,9 @@ class InLaboratoringScreen extends Component {
     }
     LaboratoryTriageChecking(requestData)
   }
-	// 显示待收费
+  // 显示待收费
   showTobeCharged() {
-    const {checking_data, pageInfo} = this.props
+    const { checking_data, pageInfo } = this.props
     console.log('checking_data====', checking_data)
     return (
       <div>
@@ -83,14 +84,18 @@ class InLaboratoringScreen extends Component {
                   <div className={'itemBottom'}>
                     <span
                       onClick={() => {
-                        // this.setState({showMask: true, selItem: patient})
+                        this.setState({ showMask: true, selItem: patient, order_status: '20' })
                       }}
-                    >检验中({patient.checking_total_count})</span>
+                    >
+                      检验中({patient.checking_total_count})
+                    </span>
                     <span
                       onClick={() => {
-                        // this.setState({showMask: true, selItem: patient})
+                        this.setState({ showMask: true, selItem: patient, order_status: '30' })
                       }}
-                    >已检验({patient.checked_total_count})</span>
+                    >
+                      已检验({patient.checked_total_count})
+                    </span>
                   </div>
                 </li>
               )
@@ -111,50 +116,62 @@ class InLaboratoringScreen extends Component {
   }
   // 加载
   render() {
+    const { selItem, order_status, showMask } = this.state
     return (
       <div>
-        <div className={'childTopBar'}>
-          <span onClick={() => Router.push('/treatment/inspect')}>待检验</span>
-          <span className={'sel'}>
-						检验中
-					</span>
-          <span onClick={() => Router.push('/treatment/inspect/checked')}>
-						已检验
-					</span>
-        </div>
-        <div className={'filterBox'}>
-          <div className={'boxLeft'}>
-            <input
-              type='date'
-              placeholder='选择开始日期'
-              value={this.state.start_date}
-              onChange={e => {
-                this.setState({start_date: e.target.value})
-              }}
-            />
-            <input
-              type='date'
-              placeholder='选择结束日期'
-              value={this.state.end_date}
-              onChange={e => {
-                this.setState({end_date: e.target.value})
-              }}
-            />
-            <input
-              type='text'
-              placeholder='搜索就诊人姓名/门诊ID/身份证号码/手机号码'
-              onClick={e => {
-                this.setState({keyword: e.target.value})
-              }}
-            />
-            <button
-              onClick={() => {
-                this.getListData({offset: 0, limit: 6})
-              }}
-            >查询</button>
+        {showMask ? (
+          <LaboraDetailScreen
+            triagePatient={selItem}
+            order_status={order_status}
+            back2List={() => {
+              this.setState({ showMask: false })
+              this.getListData({ offset: 0, limit: 6 })
+            }}
+          />
+        ) : (
+          <div>
+            <div className={'childTopBar'}>
+              <span onClick={() => Router.push('/treatment/inspect')}>待检验</span>
+              <span className={'sel'}>检验中</span>
+              <span onClick={() => Router.push('/treatment/inspect/checked')}>已检验</span>
+            </div>
+            <div className={'filterBox'}>
+              <div className={'boxLeft'}>
+                <input
+                  type='date'
+                  placeholder='选择开始日期'
+                  value={this.state.start_date}
+                  onChange={e => {
+                    this.setState({ start_date: e.target.value })
+                  }}
+                />
+                <input
+                  type='date'
+                  placeholder='选择结束日期'
+                  value={this.state.end_date}
+                  onChange={e => {
+                    this.setState({ end_date: e.target.value })
+                  }}
+                />
+                <input
+                  type='text'
+                  placeholder='搜索就诊人姓名/门诊ID/身份证号码/手机号码'
+                  onClick={e => {
+                    this.setState({ keyword: e.target.value })
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    this.getListData({ offset: 0, limit: 6 })
+                  }}
+                >
+                  查询
+                </button>
+              </div>
+            </div>
+            {this.showTobeCharged()}
           </div>
-        </div>
-        {this.showTobeCharged()}
+        )}
       </div>
     )
   }
@@ -169,4 +186,7 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { LaboratoryTriageChecking })(InLaboratoringScreen)
+export default connect(
+  mapStateToProps,
+  { LaboratoryTriageChecking }
+)(InLaboratoringScreen)
