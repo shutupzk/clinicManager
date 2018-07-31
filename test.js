beb069ce6373d455e1871caddd33c2357b2d2004
zhakang <956468883@qq.com>
2018-07-31 09:49:25 +0800

    xiugai

--- test.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs')
-var cssscss = require('css-scss')
-
-var src = fs.readFileSync('./static/css/date/antd.css', 'utf8')
-var scss = cssscss(src)
-
-fs.writeFileSync('./antd.scss', scss)
